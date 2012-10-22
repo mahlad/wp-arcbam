2c56d9d1da15ce6f8c4e5ab5d13626b35ac64f64
mahlad <mahlad365@gmail.com>
2012-10-22 21:19:01 +0330

    make this page in dynamic mode

--- wp-content/themes/arcbam-template/sidebar.php
+++ wp-content/themes/arcbam-template/sidebar.php
@@ -1,10 +1,10 @@
 <div class="sidebar">
 	<!-- <div class="pone"></div> -->
 	<ul class="puzzel">
-		<li class="pone absolute"></li>
-		<li class="ptow absolute"></li>
-		<li cLass="pthree absolute"></li>
-		<li class="pfour absolute"></li>
+		<li class="pone absolute" style="background-image:url(<?php bloginfo('template_url'); ?>/images/puzzel-1.jpg);"></li>
+		<li class="ptow absolute" style="background-image:url(<?php bloginfo('template_url'); ?>/images/puzzel-2.jpg);"></li>
+		<li cLass="pthree absolute" style="background-image:url(<?php bloginfo('template_url'); ?>/images/puzzel-3.jpg);"></li>
+		<li class="pfour absolute" style="background-image:url(<?php bloginfo('template_url'); ?>/images/puzzel-4.jpg);"></li>
 	</ul>
 	
 	<div class="clear"></div>
