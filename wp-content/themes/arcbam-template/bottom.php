fcd050761ee89bdb08c4295caf0719f397c64598
mahlad <mahlad365@gmail.com>
2012-10-22 22:41:15 +0330

    for set the name of each post type in links(it is on test)

--- wp-content/themes/arcbam-template/bottom.php
+++ wp-content/themes/arcbam-template/bottom.php
@@ -4,11 +4,18 @@
 	</div>
 	<nav class="right">
 		<ul>
-			<li class="home left"><a href="./" title="">صفحه اصلی</a> </li>
-			<li class="project left"><a href="projects.php" title="">پروژه ها</a> </li>
-			<li class="marketting left"><a href="marketting.php" title="">تبلیغات</a> </li>
-			<li class="learn left"><a href="learning.php" title="">آموزش</a> </li>
-			<li class="contact left"><a href="contact.php" title="">تماس با ما</a> </li>
+			<?php 
+			$args=array(
+				'public' => true,
+				'_builtin' =>false
+				);
+			$post_types=get_post_types($args,'names','and');
+			echo '<li class="home left"><a href="./" title="">صفحه اصلی</a> </li>';
+			foreach ($post_types as $ptype) {
+				echo '<li class="left"><a href="?task=link" title="">صفحه اصلی</a>'.$ptype.' </li>';
+			}
+			 ?>
+			
 		</ul>
 	</nav>
 
