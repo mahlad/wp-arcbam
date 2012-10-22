b6f0a685d22da34cde2c27320a9f19bd203c3ba0
mahlad <mahlad365@gmail.com>
2012-10-22 20:03:01 +0330

    make the title of each page in dynomic mode if is in home page echo description after title of site else not

--- wp-content/themes/arcbam-template/header.php
+++ wp-content/themes/arcbam-template/header.php
@@ -3,7 +3,14 @@
 <head>
 	<meta charset="UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
-	<title>بهار اندیشان معماری شرق</title>
+	<title>
+		<?php 
+		$site_description=get_bloginfo('description','display');
+		if($site_description&&(is_home()||is_front_page()))
+			echo $site_description.'-';
+		echo get_bloginfo('name');
+		 ?>
+	</title>
 	<link type="text/css"rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/1styles.css"/>
 	<link type="text/css"rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css"/>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/html5shiv.js"></script>
