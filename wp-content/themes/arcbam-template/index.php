38159a96a721712bf755de16ee1a0e9097380bf9
mahlad <mahlad365@gmail.com>
2012-10-22 22:38:29 +0330

    for omite page.php and change them to post type in dynamic mode

--- wp-content/themes/arcbam-template/index.php
+++ wp-content/themes/arcbam-template/index.php
@@ -3,9 +3,13 @@
 	template name: صفحه اصلی
 */
 	get_header();
-	get_sidebar();
+	//get_sidebar();
 	get_template_part('bottom');
+	if(!isset($_GET['task']))
+		get_template_part('home');
+	else
 	get_template_part('content','page');
+	
 ?>
 
 <?php
