f61bc29227b2d68d9949c3e793d0fa4ed28b4022
mahlad <mahlad365@gmail.com>
2012-10-22 19:07:34 +0330

    change the place for wordpress

--- wp-content/themes/arcbam-template/header.php
+++ wp-content/themes/arcbam-template/header.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 	<title>بهار اندیشان معماری شرق</title>
 	<link type="text/css"rel="stylesheet" href="css/1styles.css"/>
-	<link type="text/css"rel="stylesheet" href="css/style.css"/>
+	<link type="text/css"rel="stylesheet" href="style.css"/>
 	<script type="text/javascript" src="script/html5shiv.js"></script>
 	<script type="text/javascript" src="script/zepto.js"></script>
 	<script type="text/javascript" src="script/tile.js"></script>
