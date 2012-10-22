6b7a9a0f6eee84a32c2cc7f9eb2e356ede4a0be6
mahlad <mahlad365@gmail.com>
2012-10-22 22:37:26 +0330

    set it for seperate the link and post types

---
+++ wp-content/themes/arcbam-template/home.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+	template name: پروژه های منتخب
+*/
+ ?>
+<div class="homecontent overhidden relative">
+	<?php 
+		$path=get_bloginfo('template_url')."/images/fadeloop";
+		echo $path;
+		// $img_arr=scandir($path);
+		// foreach ($img_arr as $imag) {
+		// 	$img_type=explode('.',$imag);
+		// 	$img_type=strtolower(end($img_type));
+		// 	if($img_type=='jpg'){
+		// 	echo '<div class="fade absolute"><img src="$path/$imag" alt="$imag"></div>';
+		// 	}
+		// }
+	 ?>
+	
+</div>
