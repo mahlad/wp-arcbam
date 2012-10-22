15d70b30a228ed7eddfde35c17589e4121ec32fa
mahlad <mahlad365@gmail.com>
2012-10-22 19:17:15 +0330

    set url of link and script in header

--- wp-content/themes/arcbam-template/header.php
+++ wp-content/themes/arcbam-template/header.php
@@ -4,14 +4,14 @@
 	<meta charset="UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 	<title>بهار اندیشان معماری شرق</title>
-	<link type="text/css"rel="stylesheet" href="css/1styles.css"/>
-	<link type="text/css"rel="stylesheet" href="style.css"/>
-	<script type="text/javascript" src="script/html5shiv.js"></script>
-	<script type="text/javascript" src="script/zepto.js"></script>
-	<script type="text/javascript" src="script/tile.js"></script>
-	<script type="text/javascript" src="script/fadeloop.js"></script>
-	<script type="text/javascript" src="script/heavy-box.js"></script>
-	<script type="text/javascript" src="script/arcbam.js"></script>
+	<link type="text/css"rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/1styles.css"/>
+	<link type="text/css"rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css"/>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/html5shiv.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/zepto.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/tile.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/fadeloop.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/heavy-box.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/arcbam.js"></script>
 	
 </head>
 <body>
