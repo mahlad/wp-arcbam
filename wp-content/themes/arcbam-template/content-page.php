6756897504a6e856cd5b35bb40422e79b59587c4
mahlad <mahlad365@gmail.com>
2012-10-22 22:41:34 +0330

    (it will change)

--- wp-content/themes/arcbam-template/content-page.php
+++ wp-content/themes/arcbam-template/content-page.php
@@ -1,14 +1,1 @@
-<div class="homecontent overhidden relative">
-	<?php 
-		$path='./images/fadeloop';
-		$img_arr=scandir($path);
-		foreach ($img_arr as $imag) {
-			$img_type=explode('.',$imag);
-			$img_type=strtolower(end($img_type));
-			if($img_type=='jpg'){
-			echo "<div class='fade absolute'><img src='$path/$imag' alt='$imag'></div>";
-			}
-		}
-	 ?>
-	
-</div>
+SALAM
