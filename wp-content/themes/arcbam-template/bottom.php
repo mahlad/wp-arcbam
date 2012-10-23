4c013cf127c86fa0d1a4143660d0b63ca561ed71
mahlad <mahlad365@gmail.com>
2012-10-23 21:48:43 +0330

    it will be in dynamic mode later

--- wp-content/themes/arcbam-template/bottom.php
+++ wp-content/themes/arcbam-template/bottom.php
@@ -4,9 +4,9 @@
 	</div>
 	<nav class="right">
 		<ul>
-			<li class="home left"><a href="./" title="">صفحه اصلی</a> </li>
+			<li class="home left"><a href="http://localhost/mahlad-wp-arcbam" title="">صفحه اصلی</a> </li>
 			<li class="project left"><a href="projects.php" title="">پروژه ها</a> </li>
-			<li class="marketting left"><a href="marketting.php" title="">تبلیغات</a> </li>
+			<li class="marketting left"><a href="http://localhost/mahlad-wp-arcbam/marketting/تبلیغات" title="">تبلیغات</a> </li>
 			<li class="learn left"><a href="learning.php" title="">آموزش</a> </li>
 			<li class="contact left"><a href="contact.php" title="">تماس با ما</a> </li>
 			<?php 
