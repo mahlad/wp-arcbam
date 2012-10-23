e946de699cebccd3131e7ffd076283c9137fe27a
mahlad <mahlad365@gmail.com>
2012-10-23 13:41:22 +0330

    It is on test

--- wp-content/themes/arcbam-template/index.php
+++ wp-content/themes/arcbam-template/index.php
@@ -3,12 +3,11 @@
 	template name: صفحه اصلی
 */
 	get_header();
-	//get_sidebar();
+	get_sidebar();
 	get_template_part('bottom');
-	if(!isset($_GET['task']))
-		get_template_part('home');
-	else
-	get_template_part('content','page');
+	
+	get_template_part('home');
+	//	get_template_part('content','page');
 	
 ?>
 
