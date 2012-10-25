f308238e40307abc755e0db804f2f2bb2aeac759
mahlad <mahlad365@gmail.com>
2012-10-25 12:52:25 +0330

    it is a 404 page (for test)

--- wp-content/themes/arcbam-template/404.php
+++ wp-content/themes/arcbam-template/404.php
@@ -1,3 +1,5 @@
-<?php 
-echo 'not found';
- ?>
+<center>
+	<img src="<?php bloginfo('template_url') ?>/images/nooop.jpg">
+	<h1>I`M SORRY FOR YOU<br/>COME BACK</h1>
+	<h2><a href="http://localhost/mahlad-wp-arcbam" title="">HOME</a></h2>
+</center>
