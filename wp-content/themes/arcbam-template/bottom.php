d04a2859daff7c8942b3e476dfe63b45c6b61e0d
mahlad <mahlad365@gmail.com>
2012-10-25 11:48:25 +0330

    the url will change

--- wp-content/themes/arcbam-template/bottom.php
+++ wp-content/themes/arcbam-template/bottom.php
@@ -5,7 +5,7 @@
 	<nav class="right">
 		<ul>
 			<li class="home left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/asli.png)"><a href="http://localhost/mahlad-wp-arcbam" title="">صفحه اصلی</a> </li>
-			<li class="project left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/galery.png)"><a href="projects.php" title="">پروژه ها</a> </li>
+			<li class="project left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/galery.png)"><a href="http://localhost/mahlad-wp-arcbam/?page_id=149" title="">پروژه ها</a> </li>
 			<li class="marketting left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tablighat.png)"><a href="http://localhost/mahlad-wp-arcbam/?marketting=%d8%aa%d8%a8%d9%84%db%8c%d8%ba%d8%a7%d8%aa" title="">تبلیغات</a> </li>
 			<li class="learn left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/maghalat.png)"><a href="http://localhost/mahlad-wp-arcbam/?learn=%d8%a2%d9%85%d9%88%d8%b2%d8%b4 " title="">آموزش</a> </li>
 			<li class="contact left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tamasbama.png)"><a href="http://localhost/mahlad-wp-arcbam/?contact=%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7-%d9%85%d8%a7" title="">تماس با ما</a> </li>
