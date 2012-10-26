e4aaca1582757056f9f03a28e6eece78f93a46de
mahlad <mahlad365@gmail.com>
2012-10-26 15:04:41 +0330

    set the changes

--- wp-content/themes/arcbam-template/bottom.php
+++ wp-content/themes/arcbam-template/bottom.php
@@ -6,9 +6,9 @@
 		<ul>
 			<li class="home left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/asli.png)"><a href="http://localhost/mahlad-wp-arcbam" title="">صفحه اصلی</a> </li>
 			<li class="project left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/galery.png)"><a href="http://localhost/mahlad-wp-arcbam/?page_id=149" title="">پروژه ها</a> </li>
-			<li class="marketting left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tablighat.png)"><a href="http://localhost/mahlad-wp-arcbam/?marketting=%d8%aa%d8%a8%d9%84%db%8c%d8%ba%d8%a7%d8%aa" title="">تبلیغات</a> </li>
-			<li class="learn left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/maghalat.png)"><a href=" http://localhost/mahlad-wp-arcbam/?learn=%d8%a2%d9%85%d9%88%d8%b2%d8%b4" title="">آموزش</a> </li>
-			<li class="contact left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tamasbama.png)"><a href="http://localhost/mahlad-wp-arcbam/?contact=%d8%aa%d9%85%d8%a7%d8%b3-%d8%a8%d8%a7-%d9%85%d8%a7" title="">تماس با ما</a> </li>
+			<li class="marketting left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tablighat.png)"><a href="http://localhost/mahlad-wp-arcbam/?post_type=marketting" title="">تبلیغات</a> </li>
+			<li class="learn left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/maghalat.png)"><a href=" http://localhost/mahlad-wp-arcbam/?post_type=learn" title="">آموزش</a> </li>
+			<li class="contact left" style="background-image:url(<?php bloginfo('template_url'); ?>/images/tamasbama.png)"><a href="http://localhost/mahlad-wp-arcbam/?contact=%D8%AA%D9%85%D8%A7%D8%B3-%D8%A8%D8%A7-%D9%85%D8%A7" title="">تماس با ما</a> </li>
 			<?php 
 			// $args=array(
 			// 	'public' => true,
